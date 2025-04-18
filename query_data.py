400363425f676ff7ecaaaad6bc6d4115513f8b1b
coderFeedForwardAlg <gaspersscottma@gmail.com>
2025-04-18 14:07:30 -0700

    working rag (no mem)

--- query_data.py
+++ query_data.py
@@ -4,7 +4,6 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
@@ -16,7 +15,6 @@ You are an AI that answers questons that imployers ask about a job candadit name
 
 Answer the question based on the above context: {question}
 """
-
 
 def main():
     # Create CLI.
@@ -38,15 +36,15 @@ def main():
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
 
     model = ChatOpenAI()
     response_text = model.predict(prompt)
 
+
+
     sources = [doc.metadata.get("source", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
-
 
 if __name__ == "__main__":
     main()
